55e098e0f4a94020c3df9026d7b8445e32ee9a4f
Beulah Mathenge <beulahmathenge@gmail.com>
2022-10-24 11:36:58 +0300

    Complete

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,29 @@
+require 'pry'
 class Triangle
-  # write code here
+ attr_accessor :a, :b, :c
+
+ def initialize (a, b, c)
+  @a=a
+  @b=b
+  @c=c
+ end
+
+def kind
+ if a <= 0 || b <= 0 || c <=0 || (a + b) <= c || (b+c) <= a || (c+a) <= b
+  raise TriangleError
+ elsif
+    a==b && b==c
+  :equilateral
+ elsif 
+  a==b || b==c || c==a
+  :isosceles
+ else
+  :scalene
+ end
+end
+
+class TriangleError < StandardError
+    
+end
+
 end
