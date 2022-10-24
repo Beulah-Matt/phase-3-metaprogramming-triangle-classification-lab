9ad8fb080cd7d592987a54bf8eb3a3ea8c3c205e
Beulah Mathenge <beulahmathenge@gmail.com>
2022-10-24 11:06:00 +0300

    commit tree 1647444c47cfcd7c5c35375d5ee1b23618ee059b

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,24 @@
+require 'pry'
 class Triangle
-  # write code here
+ attr_accessor :a, :b, :c, :kind, :isosceles, :equilateral, :scalene
+
+ def initialize (a, b, c)
+  @a=a
+  @b=b
+  @c=c
+ end
+
+def kind
+  @kind= []
+ if a==b && b==c
+  kind << equilateral
+ elsif 
+  a==b || b==c || c==a
+  kind << isosceles
+ else
+  a!=b && b!=c
+  kind << scalene
+ end
+end
+
 end
